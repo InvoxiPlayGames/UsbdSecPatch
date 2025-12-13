3b9fb61364bee015a6ba156625349668d0665715
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-12-12 19:25:43 -0500

    Update build_patch to build the 17489 patch separately

--- build_patch.bat
+++ build_patch.bat
@@ -3,3 +3,5 @@ REM Set PPC_TOOLCHAIN to the path of your GCC PowerPC toolchain, including execu
 REM failkit works
 %PPC_TOOLCHAIN%-as -mregnames UsbdSecPatchXeBuild.s -o UsbdSecPatchXeBuild.bin
 %PPC_TOOLCHAIN%-objcopy UsbdSecPatchXeBuild.bin -S -O binary
+%PPC_TOOLCHAIN%-as -mregnames --defsym devkit_17489=1 UsbdSecPatchXeBuild.s -o UsbdSecPatchXeBuild17489.bin
+%PPC_TOOLCHAIN%-objcopy UsbdSecPatchXeBuild17489.bin -S -O binary
