d49fe0cf94e18776a9b6e10118eafb970c78fad8
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-12-13 12:50:08 -0500

    Update build scripts

--- build_patch.bat
+++ build_patch.bat
@@ -1,7 +1,11 @@
 @echo off
 REM Set PPC_TOOLCHAIN to the path of your GCC PowerPC toolchain, including executable prefix - e.g. C:\gcc_ppc\bin\powerpc-eabi
 REM failkit works
-%PPC_TOOLCHAIN%-as -mregnames UsbdSecPatchXeBuild.s -o UsbdSecPatchXeBuild.bin
-%PPC_TOOLCHAIN%-objcopy UsbdSecPatchXeBuild.bin -S -O binary
-%PPC_TOOLCHAIN%-as -mregnames --defsym devkit_17489=1 UsbdSecPatchXeBuild.s -o UsbdSecPatchXeBuild17489.bin
-%PPC_TOOLCHAIN%-objcopy UsbdSecPatchXeBuild17489.bin -S -O binary
+
+REM Create an output directory to store the patch files
+mkdir build
+
+for %%i in (retail_17559 devkit_17489 retail_13604 retail_9199 retail_7258 retail_6717) do (
+   %PPC_TOOLCHAIN%-as -mregnames --defsym %%i=1 UsbdSecPatchXeBuild.s -o build\UsbdSecPatchXeBuild_%%i.bin
+   %PPC_TOOLCHAIN%-objcopy build\UsbdSecPatchXeBuild_%%i.bin -S -O binary
+)
