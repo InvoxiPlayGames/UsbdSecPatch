92aa0815dcf889e42a12bd2259a2023c19300fbc
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-12-12 19:25:21 -0500

    Add offsets for 17489 devkit kernel, fix patch length calc

--- UsbdSecPatchXeBuild.s
+++ UsbdSecPatchXeBuild.s
@@ -2,13 +2,19 @@
 .set KERNEL_VIRT_BASE, 0x80000000
 
 # function patch addresses as of 17559 retail
-.set UsbdIsDeviceAuthenticated, 0x800d8748
-.set WgcAddDevice_bne, 0x800f98e0
+# and 17489 devkit
+.ifdef devkit_17489
+	.set UsbdIsDeviceAuthenticated, 0x801051b8
+	.set WgcAddDevice_bne, 0x801341f4
+.else
+	.set UsbdIsDeviceAuthenticated, 0x800d8748
+	.set WgcAddDevice_bne, 0x800f98e0
+.endif
 
 # patches UsbdIsDeviceAuthenticated to report all devices as authenticated
 # patch header
 .long UsbdIsDeviceAuthenticated - KERNEL_VIRT_BASE # patch address
-.long UsbdAuthPatchEnd - UsbdAuthPatchStart # patch length
+.long (UsbdAuthPatchEnd - UsbdAuthPatchStart) / 4 # patch length in number of DWORDs
 # patch itself - returns true
 UsbdAuthPatchStart:
 li r3, 1
@@ -18,7 +24,7 @@ UsbdAuthPatchEnd:
 # patches inside WgcAddDevice to allow for more leniency on the interface descriptor
 # patch header
 .long WgcAddDevice_bne - KERNEL_VIRT_BASE # patch address
-.long WgcIntPatchEnd - WgcIntPatchStart # patch length
+.long (WgcIntPatchEnd - WgcIntPatchStart) / 4 # patch length in number of DWORDs
 # patch itself - replaces a bne cr6, 0x10 with b 0x10 after UsbdGetEndpointDescriptor(device, 0, 3, 1)
 WgcIntPatchStart:
 b 0x10
